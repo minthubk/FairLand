56aea94b00ead5f9eaeb7ed46c09d7bb0846c7d8
B3lpheg0r <dieuset.romain@gmail.com>
2017-12-13 11:59:32 +0100

    rebuild projet

---
+++ app/src/main/java/storyland/storyland/CameraPreview.java
@@ -0,0 +1,73 @@
+package storyland.storyland;
+
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private static final String TAG = "Video";
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        mCamera = camera;
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        // deprecated setting, but required on Android versions prior to 3.0
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+
+            //mCamera.startFaceDetection(); // start face detection feature
+
+        } catch (IOException e) {
+            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+
+        if (mHolder.getSurface() == null){
+            // preview surface does not exist
+            Log.d(TAG, "mHolder.getSurface() == null");
+            return;
+        }
+
+        try {
+            mCamera.stopPreview();
+
+        } catch (Exception e){
+            // ignore: tried to stop a non-existent preview
+            Log.d(TAG, "Error stopping camera preview: " + e.getMessage());
+        }
+
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+
+            mCamera.startFaceDetection(); // re-start face detection feature
+
+        } catch (Exception e){
+            // ignore: tried to stop a non-existent preview
+            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+}
