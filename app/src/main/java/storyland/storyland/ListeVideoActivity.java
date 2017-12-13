48d7b91e12a5b80ac368fd76c57e26bab6b0bd06
B3lpheg0r <dieuset.romain@gmail.com>
2017-12-13 14:33:31 +0100

    rebuild projet

--- app/src/main/java/storyland/storyland/ListeVideoActivity.java
+++ app/src/main/java/storyland/storyland/ListeVideoActivity.java
@@ -11,7 +11,7 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-
+s
 public class ListeVideoActivity extends AppCompatActivity {
 
 
@@ -28,5 +28,27 @@ public class ListeVideoActivity extends AppCompatActivity {
         //recupere tous les fichiers contenant un certain noms
         File directory = new File(Environment.getExternalStoragePublicDirectory( Environment.DIRECTORY_PICTURES), "StoryLand");
         File[] files = directory.listFiles();
+        for( int i = 0; i < files.length; i++){
+            row = new TableRow(this); // création d'une nouvelle ligne
+
+            tv1 = new TextView(this); // création cellule
+            tv1.setText(files[i].getName()); // ajout du texte
+            //tv1.setGravity(Gravity.CENTER); // centrage dans la cellule
+            // adaptation de la largeur de colonne à l'écran :
+            tv1.setLayoutParams( new TableRow.LayoutParams( 0, android.view.ViewGroup.LayoutParams.WRAP_CONTENT, 1 ) );
+
+            // idem 2ème cellule
+            tv2 = new TextView(this);
+            tv2.setText("Partager");
+            //tv2.setGravity(Gravity.CENTER);
+            tv2.setLayoutParams( new TableRow.LayoutParams( 0, android.view.ViewGroup.LayoutParams.WRAP_CONTENT, 1 ) );
+
+            // ajout des cellules à la ligne
+            row.addView(tv1);
+            row.addView(tv2);
+
+            // ajout de la ligne au tableau
+            table.addView(row);
+        }
     }
 }
