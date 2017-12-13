446dabba4b28f816548fe13f78987f2d9ca50c05
B3lpheg0r <dieuset.romain@gmail.com>
2017-12-13 14:46:57 +0100

    rebuild projet

--- app/src/main/java/storyland/storyland/ListeVideoActivity.java
+++ app/src/main/java/storyland/storyland/ListeVideoActivity.java
@@ -10,14 +10,15 @@ import java.io.File;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.view.Gravity;
 
-s
+
 public class ListeVideoActivity extends AppCompatActivity {
 
 
-    TableLayout table = (TableLayout) findViewById(R.id.idTable); // on prend le tableau défini dans le layout
+    TableLayout table;
     TableRow row; // création d'un élément : ligne
-    TextView tv1,tv2; // création des cellules
+    TextView tv1,tv2,tv3; // création des cellules
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +29,26 @@ public class ListeVideoActivity extends AppCompatActivity {
         //recupere tous les fichiers contenant un certain noms
         File directory = new File(Environment.getExternalStoragePublicDirectory( Environment.DIRECTORY_PICTURES), "StoryLand");
         File[] files = directory.listFiles();
+        table = (TableLayout) findViewById(R.id.idTable); // on prend le tableau défini dans le layout
         for( int i = 0; i < files.length; i++){
             row = new TableRow(this); // création d'une nouvelle ligne
 
             tv1 = new TextView(this); // création cellule
             tv1.setText(files[i].getName()); // ajout du texte
-            //tv1.setGravity(Gravity.CENTER); // centrage dans la cellule
+            tv1.setGravity(Gravity.LEFT); // centrage dans la cellule
             // adaptation de la largeur de colonne à l'écran :
             tv1.setLayoutParams( new TableRow.LayoutParams( 0, android.view.ViewGroup.LayoutParams.WRAP_CONTENT, 1 ) );
 
             // idem 2ème cellule
             tv2 = new TextView(this);
             tv2.setText("Partager");
-            //tv2.setGravity(Gravity.CENTER);
+            tv2.setGravity(Gravity.CENTER);
+            tv2.setLayoutParams( new TableRow.LayoutParams( 0, android.view.ViewGroup.LayoutParams.WRAP_CONTENT, 1 ) );
+
+            // idem 3ème cellule
+            tv2 = new TextView(this);
+            tv2.setText("Supprimer");
+            tv2.setGravity(Gravity.CENTER);
             tv2.setLayoutParams( new TableRow.LayoutParams( 0, android.view.ViewGroup.LayoutParams.WRAP_CONTENT, 1 ) );
 
             // ajout des cellules à la ligne
